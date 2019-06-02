54c863b5bc7435289d33d2f99d4d3ae091768199
arunn-thevapalan <atarunn11@gmail.com>
2019-06-03 01:04:28 +0530

    Refactored the code for Maps-1

---
+++ src/main/webapp/js/maps-loader.js
@@ -0,0 +1,10 @@
+
+/*Basic function to create a Map- Part-1 */
+
+
+function createMap(){
+      const map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 37.422, lng: -122.084},
+        zoom: 16
+      });
+    }
