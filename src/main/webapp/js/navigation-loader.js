f38cf54bf33de45bfc21118f599fc72356c070cc
arunn-thevapalan <atarunn11@gmail.com>
2019-06-30 22:54:03 +0530

    minor fixes

--- src/main/webapp/js/navigation-loader.js
+++ src/main/webapp/js/navigation-loader.js
@@ -90,18 +90,17 @@ function loadContent() {
                 <li class="nav-item">
                     <a class="nav-link" href="/find-your-hobby-place.html">Search Places</a>
                 </li>
-                 <li class="nav-item">
+                <li class="nav-item">
                       <a class="nav-link" href="/charts.html">Charts</a>
-                  </li>
+                </li>
                 <li class="nav-item">
                     <a class="nav-link" href="/stats.html">Stats</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="/community.html">Community</a>
                 </li>
-                  
                 <li class="nav-item">
-                    <a class="nav-link" href="/aboutus.html">About Our Team</a>
+                    <a class="nav-link" href="/aboutus.html">Meet The Team</a>
                 </li>            
             </ul>
         </div>
